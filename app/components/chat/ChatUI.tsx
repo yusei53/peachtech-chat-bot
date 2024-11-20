f42f88fe5387a007496a8a559949cd2bf2e61f00
yusei53 <yusei530922@gmail.com>
2024-11-20 13:02:40 +0900

    fix: ChatUI

--- app/components/chat/ChatUI.tsx
+++ app/components/chat/ChatUI.tsx
@@ -1,13 +1,6 @@
-import React from "react";
-import {
-  Box,
-  InputAdornment,
-  TextField,
-  IconButton,
-  Typography,
-  CircularProgress,
-} from "@mui/material";
-import SendIcon from "@mui/icons-material/Send";
+import { Box } from "@mui/material";
+import MessageList from "./MessageList";
+import MessageInput from "./MessageInput";
 
 type Message = {
   user: string;
@@ -33,95 +26,21 @@ const ChatUI: React.FC<ChatUIProps> = ({
   isComposing,
   setIsComposing,
 }) => {
-  const handleKeyDown = (e: React.KeyboardEvent) => {
-    if (e.key === "Enter" && !isComposing) {
-      handleSendMessage();
-    }
-  };
-
   return (
     <Box
       display={"flex"}
-      justifyContent={"center"}
       alignItems={"center"}
       flexDirection={"column"}
-      position={"relative"}
-      height={"100vh"}
+      height={"95vh"}
     >
-      <Box
-        padding={2}
-        height={"70vh"}
-        width={"100%"}
-        maxWidth={"600px"}
-        borderRadius={4}
-        display={"flex"}
-        flexDirection={"column"}
-        overflow={"hidden"}
-      >
-        <Box
-          flexGrow={1}
-          overflow={"auto"}
-          sx={{
-            "&::-webkit-scrollbar": { display: "none" },
-          }}
-        >
-          {messages.map((message, index) => (
-            <Box
-              key={index}
-              textAlign={message.user === "You" ? "right" : "left"}
-              padding={1}
-            >
-              <Typography
-                bgcolor={message.user === "You" ? "#F4F4F4" : undefined}
-                padding={1}
-                borderRadius={20}
-                display={"inline-block"}
-              >
-                {message.text}
-              </Typography>
-            </Box>
-          ))}
-        </Box>
-
-        {loading && (
-          <Box textAlign={"center"} padding={1}>
-            <CircularProgress size={20} />
-          </Box>
-        )}
-      </Box>
-
-      <Box
-        display={"flex"}
-        justifyContent={"center"}
-        alignItems={"center"}
-        my={5}
-        width={"100%"}
-        maxWidth={"600px"}
-        position={"absolute"}
-        bottom={0}
-      >
-        <TextField
-          fullWidth
-          variant="outlined"
-          placeholder="メッセージを送信"
-          size="small"
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-          onCompositionStart={() => setIsComposing(true)}
-          onCompositionEnd={() => setIsComposing(false)}
-          onKeyDown={handleKeyDown}
-          InputProps={{
-            style: { borderRadius: "20px", paddingLeft: 8 },
-            endAdornment: (
-              <InputAdornment position="end">
-                <IconButton onClick={handleSendMessage}>
-                  <SendIcon />
-                </IconButton>
-              </InputAdornment>
-            ),
-          }}
-        />
-      </Box>
+      <MessageList messages={messages} loading={loading} />
+      <MessageInput
+        input={input}
+        setInput={setInput}
+        handleSendMessage={handleSendMessage}
+        isComposing={isComposing}
+        setIsComposing={setIsComposing}
+      />
     </Box>
   );
 };
